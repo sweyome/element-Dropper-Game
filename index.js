febcb784a0403543a65f87a58930bca29f6c087e
sweyome <sweyome1@gmail.com>
2020-04-03 21:53:17 -0400

    DOM element

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const message = document.querySelector('.message');
+const scoreOutput = document.querySelector(".score");
+const badLeft = document.querySelector(".badLeft");
+const btn = document.querySelector(".btn");
+const basket = document.querySelector("basket");
